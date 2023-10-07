fb54372df265b3c510d701ddc41e0f9525831c98
ARTEZON <artezon@yandex.ru>
2023-10-07 12:10:19 +0300

    Добавлены комментарии

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,11 @@
 
 int main()
 {
+    // создадим строковую переменную
     std::string name;
+    // введём имя с экрана
     std::cin >> name;
+    // выведем приветствие
     std::cout << "hello World from " << name << std::endl;
     return 0;
 }
